5d61e4ee3a60b2e9565f9f212e2e2a9d97ee31e4
chrisrekfild <chrisrekfild@mail.ru>
2024-05-11 13:49:07 +0500

    Перенос ViewModel_Base в папку Base

--- ViewModels/Base/ViewModel_Base.cs
+++ ViewModels/Base/ViewModel_Base.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProgSim.ViewModels
+namespace ProgSim.ViewModels.Base
 {
     public abstract class ViewModel_Base : INotifyPropertyChanged, IDisposable
     {
